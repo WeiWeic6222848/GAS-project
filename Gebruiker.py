5c568d738ae11cf1a118a5317d5de76b2298b544
paul-vl <33862368+paul-vl@users.noreply.github.com>
2017-12-16 14:42:42 +0100

    Update Gebruiker.py

--- Gebruiker.py
+++ Gebruiker.py
@@ -1,9 +1,9 @@
 class Gebruiker:
-    def __init__(self):
+    def __init__(self,voornaam,achternaam,email):
         self.id = None
-        self.voornaam = None
-        self.achternaam = None
-        self.email = None
+        self.voornaam = voornaam
+        self.achternaam = achternaam
+        self.email = email
         self.zoeksleutel = None
 
     def createGebruiker(self,id,voornaam,achternaam,email):
